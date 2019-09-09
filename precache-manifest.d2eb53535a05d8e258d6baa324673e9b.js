52765ade33d48501a6cdf21f38ecddcf006c8049
Vladimir Mazheru <v_mazheru@yahoo.com>
2019-09-09 12:01:07 -0400

    Updates

---
+++ precache-manifest.d2eb53535a05d8e258d6baa324673e9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab372a9d89a71cd362bc2b78219a240",
+    "url": "/vmazheru/learning_js/tree/master/complete_react_developer_in_2019/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "03493e9c8059ed4abe9b",
+    "url": "/vmazheru/learning_js/tree/master/complete_react_developer_in_2019/monsters-rolodex/static/css/main.833c271e.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/vmazheru/learning_js/tree/master/complete_react_developer_in_2019/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "03493e9c8059ed4abe9b",
+    "url": "/vmazheru/learning_js/tree/master/complete_react_developer_in_2019/monsters-rolodex/static/js/main.186e3a19.chunk.js"
+  },
+  {
+    "revision": "12a0a2a0c8ced8a862c3",
+    "url": "/vmazheru/learning_js/tree/master/complete_react_developer_in_2019/monsters-rolodex/static/js/runtime~main.87fe5c31.js"
+  }
+]);
