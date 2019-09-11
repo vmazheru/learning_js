fec96e5aa28ca302b22440151474ad5ad181fe00
Vladimir Mazheru <v_mazheru@yahoo.com>
2019-09-11 10:16:32 -0400

    Add shopping page

--- complete_react_developer_in_2019/crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ complete_react_developer_in_2019/crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl}) => (
+    <div className={`${size} menu-item`}
+      onClick={() => history.push(`${linkUrl}`)}>
       <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
       <div className='content'>
         <div className='title'>{title.toUpperCase()}</div>
@@ -11,4 +13,4 @@ const MenuItem = ({title, imageUrl, size}) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem); // add history parameter to MenuItem function
